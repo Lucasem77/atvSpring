b7452f7420e3997101c182a14fdba16c13bd54e7
Lucas Emanuel <lucasemanuel122005@gmail.com>
2024-08-13 17:23:33 -0300

    frist commit

---
+++ src/main/java/atv/static_site/StaticSiteApplication.java
@@ -0,0 +1,13 @@
+package atv.static_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atv/static_site/StaticSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package atv.static_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
